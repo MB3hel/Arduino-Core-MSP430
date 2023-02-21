0645240501521bc4d13185c7635a329959337c31
Marcus Behel <marcus.behel@mindspring.com>
2023-02-21 13:30:38 -0500

    Fixed package script

--- package.sh
+++ package.sh
@@ -6,7 +6,17 @@ printf "Version: "
 read version
 sed -i "s/^version=.*\$/version=${version}/g" platform.txt
 mkdir -p "$DIR/build/"
+rm -rf "$DIR/build/arduino-core-msp430"
+mkdir -p "$DIR/build/arduino-core-msp430"
 pushd "$DIR"
-shopt -s dotglob
-tar --exclude=arduino_core_msp430.json --exclude=build --exclude=package.sh --exclude=.git* --create --bzip2 -f "$DIR/build/msp430-core-${version}.tar.bz2" *
+cp -r cores/ build/arduino-core-msp430
+cp -r libraries build/arduino-core-msp430
+cp -r variants build/arduino-core-msp430
+cp boards.txt build/arduino-core-msp430
+cp platform.txt build/arduino-core-msp430
+cp programmers.txt build/arduino-core-msp430
+pushd "$DIR/build/"
+tar --create --bzip2 -f "$DIR/build/msp430-core-${version}.tar.bz2" arduino-core-msp430
+rm -rf arduino-core-msp430
+popd
 popd
