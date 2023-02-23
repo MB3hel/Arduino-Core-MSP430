3b1f8a81e1a68cd70aa06e4c2d8b6fb5611ed666
Marcus Behel <marcus.behel@mindspring.com>
2023-02-22 22:25:37 -0500

    Update package root directory

--- extras/package.sh
+++ extras/package.sh
@@ -6,17 +6,17 @@ printf "Version: "
 read version
 sed -i "s/^version=.*\$/version=${version}/g" platform.txt
 mkdir -p "$DIR/../build/"
-rm -rf "$DIR/../build/arduino-core-msp430"
-mkdir -p "$DIR/../build/arduino-core-msp430"
+rm -rf "$DIR/../build/mb3hel-msp430"
+mkdir -p "$DIR/../build/mb3hel-msp430"
 pushd "$DIR/.." > /dev/null
-cp -r cores/ build/arduino-core-msp430
-cp -r libraries build/arduino-core-msp430
-cp -r variants build/arduino-core-msp430
-cp boards.txt build/arduino-core-msp430
-cp platform.txt build/arduino-core-msp430
-cp programmers.txt build/arduino-core-msp430
+cp -r cores/ build/mb3hel-msp430
+cp -r libraries build/mb3hel-msp430
+cp -r variants build/mb3hel-msp430
+cp boards.txt build/mb3hel-msp430
+cp platform.txt build/mb3hel-msp430
+cp programmers.txt build/mb3hel-msp430
 pushd build/
-tar --create --bzip2 -f "msp430-core-${version}.tar.bz2" arduino-core-msp430
-rm -rf arduino-core-msp430
+tar --create --bzip2 -f "msp430-core-${version}.tar.bz2" mb3hel-msp430
+rm -rf mb3hel-msp430
 popd > /dev/null
 popd > /dev/null
