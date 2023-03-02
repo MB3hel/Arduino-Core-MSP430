db7600b33129f049874323d59509b832c904c0b1
Marcus Behel <marcus.behel@mindspring.com>
2023-03-02 01:02:38 -0500

    Outline different adcs
    - Start implementation of analogRead (two lines of meaningful code)
    - Add analog source definitions to Arduino.h

--- cores/msp430/Arduino.h
+++ cores/msp430/Arduino.h
@@ -1,9 +1,33 @@
+/**
+ * @file Arduino.h
+ * Derived from Energia MSP430 Energia.h and other energia MSP430 core files
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ * 
+ */
+
 #pragma once
 
 #include "api/ArduinoAPI.h"
 #include <msp430.h>
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Digital IO
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #ifdef __cplusplus
 // Compatability function to allow "bool" or "int" to digitalWrite
 void digitalWrite(pin_size_t pinNumber, int status);
@@ -28,6 +52,83 @@ const extern uint8_t pin_bitmask[];
 #define PxDIR(n)            (*((volatile uint8_t*)pin_dirreg[n]))
 #define PxREN(n)            (*((volatile uint8_t*)pin_renreg[n]))
 #define PxMASK(n)             ((uint8_t)pin_bitmask[n])
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Analog Inputs (ADC)
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#if defined(__MSP430_HAS_ADC10__)
+// -----------------------------------------------------------------------------------------------------------------
+// F2xx and G2xx family ADC10 module
+// -----------------------------------------------------------------------------------------------------------------
+#define DEFAULT             0
+#define INTERNAL1V5         1
+#define INTERNAL2V5         2
+#define EXTERNAL            3
+// -----------------------------------------------------------------------------------------------------------------
+#elif defined(__MSP430_HAS_ADC12__)
+// -----------------------------------------------------------------------------------------------------------------
+// F2xx and G2xx family ADC12 module
+// -----------------------------------------------------------------------------------------------------------------
+#define DEFAULT             0
+#define INTERNAL1V5         1
+#define INTERNAL2V5         2
+#define EXTERNAL            3
+// -----------------------------------------------------------------------------------------------------------------
+#elif defined(__MSP430_HAS_ADC10_B__)
+// -----------------------------------------------------------------------------------------------------------------
+// FR57xx family ADC10_B module
+// -----------------------------------------------------------------------------------------------------------------
+#define DEFAULT             0
+#define INTERNAL1V5         1
+#define INTERNAL2V0         2
+#define INTERNAL2V5         3
+#define EXTERNAL            4
+// -----------------------------------------------------------------------------------------------------------------
+#elif defined(__MSP430_HAS_ADC12_B__)
+// -----------------------------------------------------------------------------------------------------------------
+// FR58xx, FR59xx, FR6xx family ADC12_B module
+// -----------------------------------------------------------------------------------------------------------------
+#define DEFAULT             0
+#define INTERNAL1V5         1
+#define INTERNAL2V0         2
+#define INTERNAL2V5         3
+#define EXTERNAL            4
+// -----------------------------------------------------------------------------------------------------------------
+#elif defined(__MSP430_HAS_ADC__)
+// -----------------------------------------------------------------------------------------------------------------
+// FR4xx and FR2xx family ADC (multi resolution)
+// -----------------------------------------------------------------------------------------------------------------
+// Note that user-selectable 1.5V, 2.0V, and 2.5V are not available on all devices
+// But the fixed 1.5V is, so only support that one (at least for now)
+#define DEFAULT             0
+#define INTERNAL1V5         1
+#define EXTERNAL            2
+// -----------------------------------------------------------------------------------------------------------------
+#elif defined(__MSP430_HAS_ADC12_PLUS__)
+// -----------------------------------------------------------------------------------------------------------------
+// x5xx and x6xx family ADC12_A module
+// -----------------------------------------------------------------------------------------------------------------
+#define DEFAULT             0
+#define INTERNAL1V5         1
+#define INTERNAL2V0         2
+#define INTERNAL2V5         3
+#define EXTERNAL            4
+// -----------------------------------------------------------------------------------------------------------------
+#elif defined(__MSP430_HAS_ADC10_A__)
+// -----------------------------------------------------------------------------------------------------------------
+// x5xx and x6xx family ADC10_A module
+// -----------------------------------------------------------------------------------------------------------------
+#define DEFAULT             0
+#define INTERNAL1V5         1
+// #define INTERNAL2V0      2     // Supported on "non-A" only devices
+#define INTERNAL2V5         3
+#define EXTERNAL            4
+// -----------------------------------------------------------------------------------------------------------------
+#endif
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 
 #include "pins_arduino.h"
